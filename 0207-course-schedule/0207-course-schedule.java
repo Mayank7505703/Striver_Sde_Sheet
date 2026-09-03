e327daa56cab8be6295ccb695c9a1958548286a9
mayank sharma <168679681+Mayank7505703@users.noreply.github.com>
2026-09-03 15:52:39 +0530

    Time: 4 ms (91.36%), Space: 47.4 MB (12.3%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,40 @@
 class Solution {
+    public boolean dfs(int start , boolean[] vis , boolean[] path , List<List<Integer>>adj){
+        vis[start]=true;
+        path[start]=true;
+
+        for(int i: adj.get(start)){
+            if(vis[i]==false){
+                if(dfs(i , vis , path , adj)) return true;
+            }
+            else{
+                if(path[i]==true){
+                    return true;
+                }
+            }
+
+        }
+        path[start]=false;
+        return false;
+    }
     public boolean canFinish(int V, int[][] edges) {
-        List<List<Integer>>adj = new ArrayList<>();
+        List<List<Integer>>adj =new ArrayList<>();
         for(int i =0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i =0;i<edges.length;i++){
+
+        for(int i=0;i<edges.length;i++){
             int u = edges[i][0];
-            int v= edges[i][1];
-            adj.get(u).add(v);
+            int v = edges[i][1];
+            adj.get(v).add(u);
         }
-        int[] indegree=new int[V];
+        boolean[] vis =new boolean[V];
+        boolean[] path =new boolean[V];
+
         for(int i=0;i<V;i++){
-            for(int j : adj.get(i)){
-                indegree[j]++;
+            if(vis[i]==false){
+                if(dfs(i , vis, path , adj)) return false;
             }
-        }
-        Queue<Integer>q=new LinkedList<>();
-        for(int i =0;i<V;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
-        }
-        List<Integer>list = new ArrayList<>();
-        while(!q.isEmpty()){
-            int front = q.remove();
-            for(int i: adj.get(front)){
-                indegree[i]--;
-                if(indegree[i]==0){
-                    q.add(i);
-                }
-            }
-            list.add(front);
-        }
-        if(list.size()!=V){
-            return false;
         }
         return true;
     }
